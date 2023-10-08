5c5107461ff7d7b87670446bbfc3dbca74e3a7ce
Olesya <a1@mac.home>
2023-10-08 15:05:30 +0100

    mood

---
+++ lib/util/mood_item.dart
@@ -0,0 +1,10 @@
+class MoodItem {
+  final String mood;
+  final String reason;
+  final String timestamp;
+
+  MoodItem(
+      {required this.mood, required this.reason, required this.timestamp});
+
+
+}
