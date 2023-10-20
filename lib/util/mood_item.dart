75ca473cd4b899e3f2796a5f42165da8a8d1ec33
Olesya <a1@192.168.1.114>
2023-10-20 21:19:00 +0100

    comments for mood app added

--- lib/util/mood_item.dart
+++ lib/util/mood_item.dart
@@ -1,10 +1,10 @@
+// This class represents a mood-related item, storing information about a person's mood
 class MoodItem {
   final String mood;
   final String reason;
   final String timestamp;
 
+// Constructor to initialize the MoodItem with required properties
   MoodItem(
       {required this.mood, required this.reason, required this.timestamp});
-
-
 }
